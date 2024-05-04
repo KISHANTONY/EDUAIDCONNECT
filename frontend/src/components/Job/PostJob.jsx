1a514297f2fa89a0fa10cbe4c9a21f66c81359ea
KISHANTONY <kishankktony@gmail.com>
2024-05-04 21:10:49 +0530

    first commit

---
+++ frontend/src/components/Job/PostJob.jsx
@@ -0,0 +1,171 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../main";
+const PostJob = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [Gender, setGender] = useState("");
+  const [UPI, setUPI] = useState("");
+  const [city, setCity] = useState("");
+  const [location, setLocation] = useState("");
+  const [salaryFrom, setSalaryFrom] = useState("");
+  const [salaryTo, setSalaryTo] = useState("");
+  const [fixedSalary, setFixedSalary] = useState("");
+  const [salaryType, setSalaryType] = useState("default");
+
+  const { isAuthorized, user } = useContext(Context);
+
+  const handleJobPost = async (e) => {
+    e.preventDefault();
+    if (salaryType === "Fixed Salary") {
+      setSalaryFrom("");
+      setSalaryFrom("");
+    } else if (salaryType === "Ranged Salary") {
+      setFixedSalary("");
+    } else {
+      setSalaryFrom("");
+      setSalaryTo("");
+      setFixedSalary("");
+    }
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/job/post",
+        fixedSalary.length >= 4
+          ? {
+              title,
+              description,
+              Gender,
+              UPI,
+              city,
+              location,
+              fixedSalary,
+            }
+          : {
+              title,
+              description,
+              Gender,
+              UPI,
+              city,
+              location,
+              salaryFrom,
+              salaryTo,
+            },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
+  const navigateTo = useNavigate();
+  if (!isAuthorized || (user && user.role !== "Employer")) {
+    navigateTo("/");
+  }
+
+  return (
+    <>
+      <div className="job_post page">
+        <div className="container">
+          <h3>POST NEW Re</h3>
+          <form onSubmit={handleJobPost}>
+            <div className="wrapper">
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Name"
+              />
+              <select
+                value={Gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="">Gender</option>
+                <option value="Male">Male</option>
+                <option value="Mobile App Development">
+                 Female
+                </option>
+                  
+              </select>
+            </div>
+            <div className="wrapper">
+              <input
+                type="text"
+                value={UPI}
+                onChange={(e) => setUPI(e.target.value)}
+                placeholder="UPI"
+              />
+              <input
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="City"
+              />
+            </div>
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+            />
+            <div className="salary_wrapper">
+              <select
+                value={salaryType}
+                onChange={(e) => setSalaryType(e.target.value)}
+              >
+                <option value="default">Amount</option>
+                <option value="Fixed Salary">Type amount</option>
+            
+              </select>
+              <div>
+                {salaryType === "default" ? (
+                  <p>Please provide Salary Type *</p>
+                ) : salaryType === "Fixed Salary" ? (
+                  <input
+                    type="number"
+                    placeholder="Enter Amount"
+                    value={fixedSalary}
+                    onChange={(e) => setFixedSalary(e.target.value)}
+                  />
+                ) : (
+                  <div className="ranged_salary">
+                    <input
+                      type="number"
+                      placeholder="Salary From"
+                      value={salaryFrom}
+                      onChange={(e) => setSalaryFrom(e.target.value)}
+                    />
+                    <input
+                      type="number"
+                      placeholder="Salary To"
+                      value={salaryTo}
+                      onChange={(e) => setSalaryTo(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+            <textarea
+              rows="10"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Reason"
+            />
+            <button type="submit">Create Job</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostJob;
